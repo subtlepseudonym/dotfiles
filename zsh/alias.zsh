6a840a197bfc40929d2d65c8077b0961280b26dd
Connor Demille <subtlepseudonym@gmail.com>
2020-03-18 13:54:45 -0400

    Organize aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -5,16 +5,16 @@ alias ll="ls -la"
 alias lh="ls -lah"
 alias rga="rg --files --hidden"
 
-# Shortened commands
+# Docker
 alias dcc="docker-prune-containers"
 alias dci="docker-prune-images"
 alias dps="docker ps -a --format 'table {{.ID}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}\t{{.Names}}'"
 
 # Micro-scripts
-alias jq-safe="jq -R -r '. as \$line | try fromjson catch \$line'"
 alias jq-prune="jq -R -r 'try fromjson'"
-alias trim="awk '{ gsub(/^[ \t]+|[ \t]+$/, \"\"); print }'"
-alias swaplist="basename -a \"$(ls "${HOME}/.vim/tmp/swpdir")\" | sed 's#%#/#g' | sort"
-alias sl=swaplist
-alias udate="date +%s"
+alias jq-safe="jq -R -r '. as \$line | try fromjson catch \$line'"
 alias pubip="curl 'https://api.ipify.org'"
+alias sl=swaplist
+alias swaplist="basename -a \"$(ls "${HOME}/.vim/tmp/swpdir")\" | sed 's#%#/#g' | sort"
+alias trim="awk '{ gsub(/^[ \t]+|[ \t]+$/, \"\"); print }'"
+alias udate="date +%s"
