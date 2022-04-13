55052d153f52ee51f9ce8ff96e027629858c8863
Connor <subtlepseudonym@gmail.com>
2022-04-13 09:59:40 -0400

    Move docker aliases to docker module

---
+++ docker/alias.zsh
@@ -0,0 +1,3 @@
+alias dcc="docker-prune-containers"
+alias dci="docker-prune-images"
+alias dps="docker ps -a --format 'table {{.ID}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}\t{{.Names}}'"
--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,11 +6,6 @@ alias ll="ls -la"
 alias lh="ls -lah"
 alias rga="rg --files --hidden"
 
-# Docker
-alias dcc="docker-prune-containers"
-alias dci="docker-prune-images"
-alias dps="docker ps -a --format 'table {{.ID}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}\t{{.Names}}'"
-
 # Micro-scripts
 alias jq-prune="jq -R -r 'try fromjson'"
 alias jq-safe="jq -R -r '. as \$line | try fromjson catch \$line'"
