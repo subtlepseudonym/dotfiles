1202ac92842ee770d2f264154d56edef4e43bbb9
Connor <subtlepseudonym@gmail.com>
2023-09-13 12:16:25 -0400

    Add ~/.rbenv/bin to path if it exists

--- ruby/config.zsh
+++ ruby/config.zsh
@@ -1,4 +1,7 @@
+# rbenv may be in ~/.rbenv/bin or /opt/homebrew/bin
+[[ -d "${HOME}/.rbenv/bin" ]] && path=("${HOME}/.rbenv/bin" "${path[@]}")
 [[ -f "$(command -v rbenv)" ]] && eval "$(rbenv init - zsh)"
 
 # need to brew install libpq separately on apple m2 chip
+# this allows the arm64 headers for the pg lib to link properly
 [[ -d "/opt/homebrew/opt/libpq" ]] && path=("/opt/homebrew/opt/libpq/bin" "${path[@]}")
