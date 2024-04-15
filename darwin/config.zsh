043cab5adf26f6d12c1bdf03d7c26d285a7dda62
Connor <subtlepseudonym@gmail.com>
2024-04-15 09:50:39 -0400

    Fix GOROOT for brew install

--- darwin/config.zsh
+++ darwin/config.zsh
@@ -2,6 +2,10 @@ export PLAYDATE_SDK_PATH="${HOME}/Developer/PlaydateSDK"
 
 eval $(/opt/homebrew/bin/brew shellenv)
 
+if [[ -f "$(which go)" ]]; then
+	export GOROOT=$(dirname $(dirname $(readlink -f $(which go))))
+fi
+
 path=(
 	"${PLAYDATE_SDK_PATH}/bin"
 	"${path[@]}"
