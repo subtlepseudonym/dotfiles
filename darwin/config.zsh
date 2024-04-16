c1b7be16ba7fb7e27ddaf630857a92f1937bacbd
Connor <subtlepseudonym@gmail.com>
2024-04-16 09:56:54 -0400

    Revert "Fix GOROOT for brew install"
    This reverts commit 043cab5adf26f6d12c1bdf03d7c26d285a7dda62.

--- darwin/config.zsh
+++ darwin/config.zsh
@@ -2,10 +2,6 @@ export PLAYDATE_SDK_PATH="${HOME}/Developer/PlaydateSDK"
 
 eval $(/opt/homebrew/bin/brew shellenv)
 
-if [[ -f "$(which go)" ]]; then
-	export GOROOT=$(dirname $(dirname $(readlink -f $(which go))))
-fi
-
 path=(
 	"${PLAYDATE_SDK_PATH}/bin"
 	"${path[@]}"
