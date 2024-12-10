4119ee9940e9ac9a23c02d90e97e5bb9d886a486
Connor <subtlepseudonym@gmail.com>
2024-12-10 14:00:23 -0500

    Replace docker container cleanup script with alias

--- docker/alias.zsh
+++ docker/alias.zsh
@@ -1,4 +1,4 @@
-alias dcc="docker-prune-containers"
+alias dcc='docker rm $(docker ps -q -f status=created -f status=exited -f status=dead)'
 alias dci='docker rmi $(docker images -f "dangling=true" -q)'
 alias dcl="docker compose ls --all"
 alias dps="docker ps -a --format 'table {{.ID}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}\t{{.Names}}'"
