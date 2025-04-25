4dd6947854195ce77bd42a75f63b31f218c5dcba
Connor Demille <subtlepseudonym@gmail.com>
2025-04-24 21:13:49 -0400

    Fix NVM_DIR path

--- node/config.zsh
+++ node/config.zsh
@@ -1,4 +1,5 @@
 export NVM_DIR="${HOME}/.nvm"
 export NVM_LAZY_LOAD=true
 
-[ -f "/usr/share/nvm/init-nvm.sh" ] && source "/usr/share/nvm/init-nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
